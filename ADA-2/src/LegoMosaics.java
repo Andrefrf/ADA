b3bb93a10d63ccbadd56aa6bbbad0a75a436b34d
Andrefrf <andrefiliperferreira@hotmail.com>
2018-03-18 14:28:19 +0000

    repostiory updated

--- ADA-2/src/LegoMosaics.java
+++ ADA-2/src/LegoMosaics.java
@@ -1,21 +1,67 @@
 
 public class LegoMosaics {
-	
-	static int columns;
+
+	static int columns/* = 1000 */;
+	static int[] block = new int[] { 1, 2, 3, 4, 6, 8, 10, 12, 16 };
+
 	int[] repetitions;
 	int[] combinations;
-	static int[] symbols;
 	int biggestSeq;
-	
+
 	public LegoMosaics(int columns) {
 		this.columns = columns;
-		
+		this.repetitions = new int[columns + 1];
+		this.combinations = new int[columns + 1];
 	}
 
-	void addLine() {}
-	
-	void addBlock() {}
-	
-	void calc() {}
-	
+	void addLine(String spots) {
+
+		char last = spots.charAt(0);
+		int blockSize = last == '.' ? 0 : 1;
+
+		for (int x = 1; x < columns; x++) {
+
+			char current = spots.charAt(x);
+			
+			if (last != '.' && current != last) {
+				repetitions[blockSize]++;
+				blockSize = (current == '.') ? 0 : 1;
+			} else {
+				if (current != '.') {
+					blockSize++;
+					biggestSeq = Math.max(blockSize, biggestSeq);
+				}
+			}
+			last = current;
+		}
+		if (blockSize > 0) {
+			repetitions[blockSize]++;
+		}
+	}
+
+	int calc() {
+		int result = 1;
+
+		for (int i = 1; i <= biggestSeq; i++) {
+			result *= Math.pow(combinations[i], repetitions[i]);
+		}
+
+		return result;
+	}
+
+	void fillCombi() {
+		combinations[0] = 1;
+		for (int i = 1; i <= biggestSeq; i++) {
+			combinations[i] = sumPrev(i);
+		}
+	}
+
+	int sumPrev(int i) {
+		int sumP = 0;
+		for (int j = 0; j < block.length && block[j] <= i; j++) {
+			sumP += combinations[i - block[j]];
+		}
+		return sumP;
+	}
+
 }
--- ADA-2/src/Main.java
+++ ADA-2/src/Main.java
@@ -1,20 +1,24 @@
 import java.io.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
-		char[] line = in.readLine().toCharArray();
-		int rows = line[0];
-		int columns = line[2];
-		
+
+		String line = in.readLine();
+		int rows = Character.getNumericValue(line.charAt(0));
+		int columns = Character.getNumericValue(line.charAt(2));
+
 		LegoMosaics lego = new LegoMosaics(columns);
+
+		for (int i = 1; i < rows; i++) {
+			lego.addLine(in.readLine());
+		}
 		
-		
-		
-		
+		lego.fillCombi();
+		System.out.println(lego.calc());
+
 	}
 
 }
